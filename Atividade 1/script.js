780097c503266ec1463fe8b1e422da598bb16631
Henrique <henriquefernandes.gouveia@gmail.com>
2021-09-16 21:28:04 -0300

    aula-js

---
+++ Atividade 1/script.js
@@ -0,0 +1,15 @@
+//var
+var nome = "Henrique";
+var sobrenome = "Fernandes";
+
+//let
+let idade = 22;
+let sexo = "M";
+
+//const
+const cpf = "000.000.000-00";
+const data_de_nascimento = "13.08.1999";
+
+
+console.log(`Olá, ${nome},${sobrenome}, Você tem ${idade} anos, seu sexo é: "${sexo}" seu cpf é: ${cpf}, e sua 
+data de nascimento é ${data_de_nascimento}`);
---
+++ Atividade 2/atividade1.js
@@ -0,0 +1,14 @@
+//Number
+var numero = 2;
+//String
+var nome = "Henrique";
+//Boolean
+var moraEmRecife = true;
+//Array
+var carro = ["onix","celta","civic"];
+//Object
+var casa = {cor:"amarela", cidade:"Recife", podeFinanciar:"true"};
+//Undefined
+var indefinido;
+//Null
+var vazio = null;
